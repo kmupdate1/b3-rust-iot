dc95f2733e3438c7a3f463e14aa45ab79e1a4d38
Ken <lax.potcx1562@gmail.com>
2026-06-25 23:09:05 +0900

    rp235x系アナログピンドライバ実装

--- hal/rp235x/src/adc.rs
+++ hal/rp235x/src/adc.rs
@@ -1,11 +1,11 @@
 use hal_traits::analog_in::AnalogIn;
 
-struct RpAdcPin<'a, T: embassy_rp::adc::AdcChannel> {
+pub struct AdcPin<'a, T: embassy_rp::adc::AdcChannel> {
     channel: T,
     adc: &'a mut embassy_rp::adc::Adc<'static, embassy_rp::adc::Async>,
 }
 
-impl<'a, T: embassy_rp::adc::AdcChannel> AnalogIn for RpAdcPin<'a, T> {
+impl<'a, T: embassy_rp::adc::AdcChannel> AnalogIn for AdcPin<'a, T> {
     type Error = embassy_rp::adc::Error;
 
     async fn read(&mut self) -> Result<u16, Self::Error> {
@@ -13,11 +13,25 @@ impl<'a, T: embassy_rp::adc::AdcChannel> AnalogIn for RpAdcPin<'a, T> {
     }
 }
 
-impl <'a, T> RpAdcPin<'a, T> {
+impl<'a, T> AdcPin<'a, T> {
     pub fn new(
         channel: T,
         adc: &'a mut embassy_rp::adc::Adc<'static, embassy_rp::adc::Async>,
     ) -> Self {
         Self { channel, adc }
+    }
+}
+
+pub struct AdcDriver {
+    adc: embassy_rp::adc::Adc<'static, embassy_rp::adc::Async>,
+}
+
+impl AdcDriver {
+    pub fn new(adc: embassy_rp::adc::Adc<'static, embassy_rp::adc::Async>) -> Self {
+        Self { adc }
+    }
+
+    pub fn create_pin<T: embassy_rp::adc::AdcChannel>(&mut self, channel: T) -> AdcPin<'_, T> {
+        AdcPin::new(channel, &mut self.adc)
     }
 }
