89d634e0a9aa2b3fa5775ee20b995390f8e626b7
Ken Murase <lax.potcx1562@gmail.com>
2026-07-18 17:23:41 +0900

    途中やけど一旦コミット・プッシュ

--- examples/analog/src/main.rs
+++ examples/analog/src/main.rs
@@ -1,6 +1,7 @@
+use embassy_rp::config::Config;
 use hal_rp235x::adc::AdcDriver;
 use hal_rp235x::pins::Gpio26;
 
 fn main() {
-    let gpio26 = AdcDriver::init(embassy_rp)
+    let gpio26 = AdcDriver::init(embassy_rp::init(Config::config()));
 }
