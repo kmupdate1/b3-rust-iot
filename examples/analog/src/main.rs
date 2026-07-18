55bf893898d29e94a832a732577ad235006c4bbf
Ken <lax.potcx1562@gmail.com>
2026-07-18 17:57:45 +0900

    全体構成再編

--- examples/analog/src/main.rs
+++ examples/analog/src/main.rs
@@ -1,6 +1,6 @@
 use embassy_rp::config::Config;
-use hal_rp235x::adc::AdcDriver;
-use hal_rp235x::pins::Gpio26;
+use rp235x::adc::AdcDriver;
+use rp235x::pins::Gpio26;
 
 fn main() {
     let gpio26 = AdcDriver::init(embassy_rp::init(Config::config()));
