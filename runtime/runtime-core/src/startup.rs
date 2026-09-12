cd05f059b3cb58361a46026b90652d68ab23e3b7
Ken <lax.potcx1562@gmail.com>
2026-09-12 23:27:04 +0900

    function->capability

---
+++ runtime/runtime-core/src/startup.rs
@@ -0,0 +1,19 @@
+use crate::Ota;
+
+pub trait Startup {
+    async fn check<O>(
+        ota: &mut O,
+        current_v: &str,
+    ) -> Result<bool, O::Error>
+    where
+        O: Ota,
+    {
+        if !ota.available(current_v).await? {
+            return Ok(false);
+        }
+        
+        ota.update().await?;
+        
+        Ok(true)
+    }
+}
